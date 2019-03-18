ce83ba12e09d259d350f6c90e33482223cc0363a
pascalnitsch <48013432+pascalnitsch@users.noreply.github.com>
2019-03-18 14:35:42 +0100

    Add files via upload

---
+++ quizzerOHNE/src/quizzer/Quizzer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizzer;
+
+/**
+ *
+ * @author pasca
+ */
+public class Quizzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //start quizz = new start();
+        //NewJFrame quiz = new NewJFrame();
+       // quiz.setVisible(true);
+        startbild jFrame = new startbild();
+        jFrame.setVisible(true);
+        jFrame.setTitle("Quizzer - Masse statt Klasse");
+        
+        //quizz.setVisible(true);
+        
+        
+    }
+    
+}
